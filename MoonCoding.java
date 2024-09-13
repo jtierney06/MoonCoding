41c58a1178345976e7dc7811906800ef853f2dca
jtierney06 <jamespt06@icloud.com>
2024-09-13 14:41:02 -0400

    Create MoonCoding.java

---
+++ MoonCoding.java
@@ -0,0 +1,7 @@
+public class MoonCoding{
+    public static void main(String[] args){
+        double val = -0.7;
+        int roundedVal = (int)(val + 0.5);
+        System.out.println(roundedVal);
+    }
+}
